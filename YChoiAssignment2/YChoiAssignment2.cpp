6395c5e3b2d9c1d4a59ca854afe678a39fe78f0c
Tifare <choiyt@hotmail.com>
2017-10-12 23:13:37 -0400

    first

--- YChoiAssignment2/YChoiAssignment2.cpp
+++ YChoiAssignment2/YChoiAssignment2.cpp
@@ -2,10 +2,32 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
+char translateToPigLatin(char myphr[50]);
 
 int main()
 {
+	char myphrase[50];
+	cout << "Enter phrase " << endl;
+	gets_s(myphrase);
+	char phrase = translateToPigLatin(myphrase);
+	cout << "Translated to Pig Latin " << phrase << endl;
+	
     return 0;
 }
+
+
+char translateToPigLatin(char myphr[50])
+{
+	int count = 0;
+	
+	
+
+	return count;
+}
+
+
+
 
